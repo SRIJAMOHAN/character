3b476ba12ccfe99e01090117788fd1c28dbd0154
SRIJAMOHAN <30283031+SRIJAMOHAN@users.noreply.github.com>
2017-09-28 14:33:49 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char cH;
+scanf("%c",c);
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+{
+printf("It is a vowel");
+}
+else
+{
+printf("It is constant");
+}
+]
